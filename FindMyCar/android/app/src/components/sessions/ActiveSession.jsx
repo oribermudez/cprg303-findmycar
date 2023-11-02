677bb0e154b0e22e94fbdcfc07cf4af0056e7f79
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2023-11-02 12:01:03 -0600

    Initial commit

--- FindMyCar/android/app/src/components/sessions/ActiveSession.jsx
+++ FindMyCar/android/app/src/components/sessions/ActiveSession.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import {
+  Card,
   Divider,
   Icon,
-  Layout,
   Text,
   TopNavigation,
   TopNavigationAction,
@@ -31,10 +31,13 @@ const ActiveSessionScreen = ({ navigation }) => {
         accessoryLeft={BackAction}
       />
       <Divider />
-      <Layout
-        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text category="h1">DETAILS</Text>
-      </Layout>
+      <Card>
+    <Text>
+      The Maldives, officially the Republic of Maldives, is a small country in South Asia,
+      located in the Arabian Sea of the Indian Ocean.
+      It lies southwest of Sri Lanka and India, about 1,000 kilometres (620 mi) from the Asian continent
+    </Text>
+  </Card>
     </SafeAreaView>
   );
 };
