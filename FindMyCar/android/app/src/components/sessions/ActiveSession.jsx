c7c8a85cfd8d7c06652b7dbb6542ac4d9a9141d1
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2023-11-10 13:17:23 -0700

    create map card

--- FindMyCar/android/app/src/components/sessions/ActiveSession.jsx
+++ FindMyCar/android/app/src/components/sessions/ActiveSession.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, View, StyleSheet } from 'react-native';
+import { SafeAreaView, View, StyleSheet, Image } from 'react-native';
 import {
   Card,
   Divider,
@@ -33,42 +33,62 @@ const ActiveSessionScreen = ({ navigation }) => {
       />
       <Divider />
 
-      <Card>
-    <Text>
-      The Maldives, officially the Republic of Maldives, is a small country in South Asia,
-      located in the Arabian Sea of the Indian Ocean.
-      It lies southwest of Sri Lanka and India, about 1,000 kilometres (620 mi) from the Asian continent
-    </Text>
-  </Card>
-
-      <Layout
-        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text category="h1">HOLAA</Text>
-      </Layout>
-
       <View style={styles.carView}>
-      <View style={styles.aliasContainer}>
-            <Text style={styles.alias}>Tony's Car </Text>
-            {favorite && (
-              <Icon fill="#FFC10F" name="star" style={styles.icon} />
-            )}
+        <View style={styles.aliasContainer}>
+          <Text style={styles.alias}>Tony's Car </Text>
+          {favorite && (
+            <Icon fill="#FFC10F" name="star" style={styles.icon} />
+          )}
+          <Text style={styles.alias}>CPRG303</Text>
+        </View>
+        
+      </View>
+
+      {/* Wrapping view for everything  */}
+      <View style={styles.wrappingView}>
+      <View style={styles.parkingTimeView}>
+        <Text style={styles.parkingTimeText}>PARKING TIME</Text>
+        <Text style={styles.parkingTime}>01:39:00</Text>
+      </View>
+
+      <View style={styles.zoneFeeStatusContainer}>
+        <Text style={styles.zoneText}> ZONE </Text>
+        <Text style={styles.feeText}> FEE PER HOUR </Text>
+        <Text style={styles.statusText}> STATUS </Text>
+
+      </View>
+      <Card style={styles.cardMap}>
+        <View style={styles.mapViewContainer}>
+          <View style={styles.mapContainer} >
+            <Text style={styles.locationText}>LOCATION</Text>
+            <Text style={styles.addressText}>1234 Main St. Vancouver BC</Text>
           </View>
-  </View>
+          <View >
+            <Image source={require('./map.png')} style={styles.mapImage} />
+          </View>
+        </View>
+      </Card>
+      </View>
 
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  wrappingView:{
+    padding: 20,
+
+  },
   carView: {
     backgroundColor: '#20C5B1',
     width: '100%',
-    height: 176
+    height: 176,
   },
   aliasContainer: {
     display: 'flex',
     flexDirection: 'row',
     marginBottom: 7,
+    spaceBetween: 'space-between',
   },
   alias: {
     fontFamily: 'Open Sans',
@@ -81,6 +101,56 @@ const styles = StyleSheet.create({
     width: 17,
     marginLeft: 10,
   },
+  mapImage: {
+    width: 50,
+    height: 50,
+  },
+  cardMap: {
+    backgroundColor: '#FFFFFF',
+    width: '100%',
+    height: 100,
+    padding: 10,
+  },
+  mapViewContainer:{
+    flexDirection: 'row',
+
+    
+  },
+  locationText:{
+    fontFamily: 'Open Sans',
+    color: '#8F9BB3',
+    fontSize: 10,
+  },
+  addressText:{
+    color: '#000000',
+    fontSize: 12,
+  },
+  mapContainer:{
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  parkingTimeView:{
+    width: '80%',
+    margin: 40,
+    height: 50,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  parkingTimeText:{
+    fontFamily: 'Open Sans',
+    color: '#8F9BB3',
+    fontSize: 12,
+  },
+  parkingTime:{
+    fontFamily: 'Open Sans',
+    fontWeight: '700',
+    //space between the numbers
+    letterSpacing: 3,
+    color: '#000000',
+    fontSize: 30,
+  },
+  
 });
 
 export default ActiveSessionScreen;
